4086e82d6ef0d632d77fa1f5d59e72a9575ace56
nahidul77 <nahidul.mee@gmail.com>
2023-02-25 16:09:08 +0600

    resto project init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <SingUp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SingUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SingUp
   }
 }
 </script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img class="logo" src="../assets/resto-logo.jpg" />
+        <h3>Sign Up</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "SignUp",
+    
+}
+</script>
+<style>
+    .logo {
+        width: 100px;
+    }
+</style>
